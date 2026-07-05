979531bc6f766e306ff13b43bf7d108de7c4bcee
RITURAJKUMAR <riturajkumar1105@gmail.com>
2026-07-05 12:46:59 +0530

    Initial folder structure(Frontend & Backend

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -2,4 +2,5 @@ from fastapi import FastAPI
 from fastapi.middleware import CORSMiddleware
 
 app = FastAPI()
+
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
