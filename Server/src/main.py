95eefa903f38121c8e8d33928465009bf4b873eb
disha01bansal <disha.bansal2005@gmail.com>
2026-07-10 18:23:41 +0530

    Remove merge head

--- Server/src/main.py
+++ Server/src/main.py
@@ -36,10 +36,9 @@ app.include_router(controller.router, prefix=settings.API_PREFIX)
 app.include_router(user_controller.router, prefix=settings.API_PREFIX)
 app.include_router(notification_controller.router, prefix=settings.API_PREFIX)
 app.include_router(audit_logs_controller.router, prefix=settings.API_PREFIX)
-<<<<<<< HEAD
+
 app.include_router(renewals_controller.router, prefix=settings.API_PREFIX)
-=======
->>>>>>> origin/main-group-A
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
