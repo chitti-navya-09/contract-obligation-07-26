79a08e70547a806fa7337931f3a1d4de7f09af46
sireesha <sireepaddhu@gmail.com>
2026-07-13 20:52:54 +0530

    Fix password hash and api url

--- Server/src/main.py
+++ Server/src/main.py
@@ -38,4 +38,4 @@ app.include_router(renewals_controller.router, prefix=settings.API_PREFIX)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
