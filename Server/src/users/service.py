d2b32cfbf4372c4c472063cdbffd5141b9e15d44
RITURAJKUMAR <riturajkumar1105@gmail.com>
2026-07-14 14:28:41 +0530

    User-Management

--- Server/src/users/service.py
+++ Server/src/users/service.py
@@ -12,7 +12,6 @@ def admin_required(
     db: Session = Depends(get_db),
 ):
     user = db.query(User).filter(User.email == payload["sub"]).first()
-
     if not user:
         raise HTTPException(404, "User not exist!!")
 
@@ -21,5 +20,4 @@ def admin_required(
         UserRole.LEGAL_MANAGER,
     ]:
         raise HTTPException(403, "Access denied!!")
-
     return user
