305ab6ea839f4c2ca6dcf7d026390d4fbb9b7003
Deepa <deepach0722@gmail.com>
2026-07-14 16:59:28 +0000

    Merge main-group-C and resolve conflicts

--- server/src/main.py
+++ server/src/main.py
@@ -5,18 +5,24 @@ from src.api import api_router
 from src.logging import configure_logging
 from src.rate_limiter import init_rate_limiter
 
+
 configure_logging()
 
-app = FastAPI(title="Server")
+app = FastAPI(title="ContractIQ")
 
 app = init_rate_limiter(app)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://automatic-pancake-5g7qvwj9v6x735jj-3000.app.github.dev",
         "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:3001",
+        "http://127.0.0.1:3001",
+        "http://localhost:3002",
+        "http://127.0.0.1:3002",
     ],
+    allow_origin_regex=r"https://.*\.app\.github\.dev",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
