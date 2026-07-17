2403b27a668195918b90055859a8c57d7f83c981
Edagotti <chittinavya09@gmail.com>
2026-07-17 15:13:38 +0530

    Fix merge conflict markers in reportWebVitals.js

--- client/src/reportWebVitals.js
+++ client/src/reportWebVitals.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-const reportWebVitals = (onPerfEntry) => {
-  // No-op by default. Wire up the `web-vitals` package here if you
-  // later want to measure Core Web Vitals.
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    // intentionally left blank
-=======
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
@@ -14,7 +7,6 @@ const reportWebVitals = onPerfEntry => {
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
     });
->>>>>>> upstream/main-group-B
   }
 };
 
